30b90d54e58cf7f9e833ed5bf2b87ed0571850d4
Montana Ruth <montanalruth@gmail.com>
2015-07-28 09:40:25 -0500

    Solved Problem 9

--- src/euler/Euler9.scala
+++ src/euler/Euler9.scala
@@ -4,5 +4,15 @@ package euler
  * Created by Montana Ruth on 7/28/2015.
  */
 object Euler9 extends App {
+  def isTriplet(a: Int, b: Int, c: Int, sum: Int) =
+  {
+    (a*a) + (b * b) == (c*c) &&
+      a+b+c == sum &&
+      a < b && b < c
+  }
 
+  val triplets = for(a <- 1 to 1000; b <- a to 1000; c <- b to 1000 if(isTriplet(a,b,c,1000))) yield (a,b,c)
+
+  val ans = triplets.head._1 * triplets.head._2 * triplets.head._3
+  println(ans)
 }
