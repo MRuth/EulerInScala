10e32262927553fd5b1b2852a147173cebe065c2
Montana Ruth <montanalruth@gmail.com>
2016-07-11 15:52:43 -0500

    Renamed input files and references

--- src/euler/Euler13.scala
+++ src/euler/Euler13.scala
@@ -6,5 +6,5 @@ import scala.io.Source
  * Created by Montana Ruth on 7/28/2015.
  */
 object Euler13 extends App {
-  println(Source.fromFile("Files/Euler13In.txt").getLines.map(BigInt(_)).sum.toString.take(10 ))
+  println(Source.fromFile("Files/Euler13.in").getLines.map(BigInt(_)).sum.toString.take(10 ))
 }
--- src/euler/Euler22.scala
+++ src/euler/Euler22.scala
@@ -6,6 +6,6 @@ import scala.io.Source
  * Created by Montana Ruth on 7/29/2015.
  */
 object Euler22 extends App {
-  val names = Source.fromFile("Files/Euler22Names.txt").getLines.mkString.replaceAll("\"","").split(",").sorted.toList
+  val names = Source.fromFile("Files/Euler22.in").getLines.mkString.replaceAll("\"","").split(",").sorted.toList
   println(names.zip(Stream.from(1)).map{t => t._1.map(_ - 'A' + 1).sum * t._2}.sum)
 }
